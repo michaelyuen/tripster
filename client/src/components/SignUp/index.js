f072bf1af6426e4dc51100c1c7809393ee77661b
sue <sueann.ioanis@gmail.com>
2020-01-18 22:27:29 -0500

    Bugfix imports (#4)

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from "react-router-dom";
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Typography, Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { Link } from "react-router-dom";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
 
 const useStyles = makeStyles(theme => ({
   paper: {
